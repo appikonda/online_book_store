fbe73694fc353c4efc5b52b3ad5cbf9e0fac52d0
Sandeep Appikonda <sandeep@appikonda.in>
2014-12-04 23:11:32 -0800

    Update database creds

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'obs';
-$db['default']['password'] = 'appikonda';
-$db['default']['database'] = 'obs';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'online_book_store';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
